b7366ac4082ab7dcb22f370a29b8309f7d8f2715
Vallerboy <10135886>
2016-09-07 18:05:30 +0200

    Initial commit

---
+++ app/src/main/java/pl/akademiakodu/asynctask/MainActivity.java
@@ -0,0 +1,19 @@
+package pl.akademiakodu.asynctask;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import java.util.Arrays;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
