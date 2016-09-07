a71243507a2c180d29fcfe2f263186b2350e5e77
Vallerboy <10135886>
2016-09-07 20:52:27 +0200

    First Commit6

--- app/src/main/java/pl/akademiakodu/asynctask/MainActivity.java
+++ app/src/main/java/pl/akademiakodu/asynctask/MainActivity.java
@@ -4,8 +4,11 @@ package pl.akademiakodu.asynctask;
 import android.animation.AnimatorSet;
 import android.graphics.drawable.AnimationDrawable;
 import android.media.Image;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ArrayAdapter;
@@ -29,14 +32,48 @@ public class MainActivity extends AppCompatActivity  {
     public RelativeLayout layout;
 
 
+    public final static int RUN_ANIMATION = 1;
+
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        AnimationDrawable animation = (AnimationDrawable) layout.getBackground();
-        animation.start();
+
+
+        final AnimationDrawable animation = (AnimationDrawable) layout.getBackground();
+      //  animation.start();
+
+
+        Handler handler = new Handler(new Handler.Callback() {
+            @Override
+            public boolean handleMessage(Message msg) {
+                 if(msg.what == 1) {
+                     animation.start();
+                 }else if(msg.what == 2) {
+                     animation.stop();
+                 }
+                //Log.e("wiadomosc", "to sie nie powinno w ogole wyswietlic");
+                return false;
+            }
+        });
+
+        //handler.sendEmptyMessageDelayed(1, 5000);
+        //handler.sendEmptyMessageDelayed(2, 10000);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                animation.start();
+            }
+        }, 5000);
+
     }
 
 }
